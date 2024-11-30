76bc9bdae6236dfa84984ed82c77d0c74dd21512
AlanxEpic <108180019+AlanxEpic@users.noreply.github.com>
2024-11-30 09:52:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ api_hash = "4d83e959108956d7c0b05bd8f52f54b5"
 STRING_SESSION = os.environ.get("STRING_SESSION")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ru = random.randint(182763637281, 82828272726525262)
+OWNER = (6106882014, 5644071668)
 user = Client("user", api_id=api_id, api_hash=api_hash, session_string=STRING_SESSION, in_memory=True)
 bot = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=BOT_TOKEN)
 
@@ -25,7 +26,7 @@ def home():
 async def deval(client, message):
     me = await client.get_me()
     print(me)
-    if not message.from_user.id == me.id:
+    if not message.from_user.id == OWNER_ID:
         return
     status_message = await message.channel.send("**•×• Processing... •×•**")
     cmd = message.content.split(" ", maxsplit=1)[1]
